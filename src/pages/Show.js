6b2b64ed4f7e5d8414fc14333e93b10246ab6e33
Ruhama Yared <ruhamayared1@gmail.com>
2023-01-10 20:13:42 -0500

    finshed with CRUD

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,28 @@
+import { useLoaderData, Form } from "react-router-dom"
+
 function Show(props) {
-  return <h1>Show Page</h1>
+  const person = useLoaderData()
+
+  return (
+    <div className="person">
+      <h1>{person.name}</h1>
+      <h2>{person.title}</h2>
+      <img src={person.image} alt={person.name} />
+
+      <h2>Update {person.name}</h2>
+      <Form action={`/update/${person._id}`} method="post">
+        <input type="input" name="name" placeholder="person's name" />
+        <input type="input" name="image" placeholder="person's picture" />
+        <input type="input" name="title" placeholder="person's title" />
+        <input type="submit" value={`Update ${person.name}`} />
+      </Form>
+
+      <h2>Delete Person</h2>
+      <Form action={`/delete/${person._id}`} method="post">
+        <input type="submit" value={`Delete ${person.name}`} />
+      </Form>
+    </div>
+  )
 }
 
 export default Show
--- src/router.js
+++ src/router.js
@@ -2,17 +2,17 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import App from "./App"
 import Index from "./pages/Index"
 import Show from "./pages/Show"
-import { peopleLoader } from "./loaders"
-import { createAction } from "./actions"
+import { peopleLoader, personLoader } from "./loaders"
+import { createAction, updateAction, deleteAction } from "./actions"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Index />} loader={peopleLoader} />
-      <Route path=":id" element={<Show />} />
+      <Route path=":id" element={<Show />} loader={personLoader} />
       <Route path="create" action={createAction} />
-      <Route path="update/:id" />
-      <Route path="delete/:id" />
+      <Route path="update/:id" action={updateAction} />
+      <Route path="delete/:id" action={deleteAction} />
     </Route>
   )
 )
